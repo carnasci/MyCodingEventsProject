ce5b08b10514fd112568d25e5d62dec999022fc2
carnasci <cen.master24@gmail.com>
2024-01-23 13:42:05 -0600

    finished adding delete event functionality

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -50,4 +50,14 @@ public class EventController {
         return "event/delete";
     }
 
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:";
+    }
+
 }
